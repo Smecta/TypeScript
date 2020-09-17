c2baa856c0e9d087a95e20f5d19bd24e77ee3bdf
Smecta <chchengeng@gmail.com>
2020-09-17 23:36:14 +0800

    更新break和continue P54完结

--- test.ts
+++ test.ts
@@ -1,28 +1,45 @@
-// let isVip: boolean = true;
+// // let isVip: boolean = true;
 
-// if (isVip) {
-//   console.log("是vip会员，可以观看完整电影");
-// } else {
-//   console.log("不是vip会员，试看5分钟");
+// // if (isVip) {
+// //   console.log("是vip会员，可以观看完整电影");
+// // } else {
+// //   console.log("不是vip会员，试看5分钟");
+// // }
+
+// let age:number = 18
+
+// if (age < 18) {
+//     console.log('回家看熊出没')
+// } else{
+//     console.log('玩吃鸡好爽啊')
 // }
 
-let age:number = 18
+// // let result:string = 5 < 3 ? "大于" : "小于"
+// // console.log(result)
 
-if (age < 18) {
-    console.log('回家看熊出没')
-} else{
-    console.log('玩吃鸡好爽啊')
-}
+// let result:string = age < 18 ? '回家看熊出没' : '玩刺激好爽啊'
 
-// let result:string = 5 < 3 ? "大于" : "小于"
 // console.log(result)
 
-let result:string = age < 18 ? '回家看熊出没' : '玩刺激好爽啊'
+// let love:string = '我爱你三千遍'
+// for ( let i:number = 0; i < 3; i++){
+//     console.log(i)
+//     console.log(love)
+// }
 
-console.log(result)
+// for (let i: number = 1; i <= 5; i++) {
+//     if ( i === 3){
+//         break
+//     }
 
-let love:string = '我爱你三千遍'
-for ( let i:number = 0; i < 3; i++){
-    console.log(i)
-    console.log(love)
+//   console.log('正在吃第'+i+'个包子');
+// }
+
+for (let i: number = 1; i <= 5; i++) {
+    if ( i === 3){
+        continue
+    }
+
+  console.log('正在吃第'+i+'个包子');
 }
+
